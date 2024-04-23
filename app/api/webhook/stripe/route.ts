fa45b5d30acbf26aef1bbd14ecf2d4b4612f2481
Keith Rincon <keithrt3008@gmail.com>
2024-04-23 00:06:43 -0700

    got it

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request) {
 
     const order = {
       stripeId: id,
-      teamId: metadata?.teamId || '',
+      eventId: metadata?.eventId || '',
       buyerId: metadata?.buyerId || '',
       totalAmount: amount_total ? (amount_total / 100).toString() : '0',
       createdAt: new Date(),
